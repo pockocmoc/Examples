ff4de90a4770ca788b519250cb153e4c1735fa75
pockocmoc <pockocmoc@inbox.ru>
2022-09-13 15:02:33 +0300

    Еxample003_Sum

---
+++ Еxample003_Sum/Program.cs
@@ -0,0 +1,8 @@
+﻿// int numberA = 3;
+// int numberB = 5;
+// Console.WriteLine(numberA + numberB);
+
+int numberA = 31;
+int numberB = 5;
+int result = numberA + numberB;
+Console.WriteLine(result);
