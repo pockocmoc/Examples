9f17296e6a79bd630c0b7f9bdde2510e5307dab8
pockocmoc <pockocmoc@inbox.ru>
2022-09-13 16:23:07 +0300

    add Еxample 005 Condition If-Else

---
+++ Еxample005_ConditionIfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string name = Console.ReadLine();
+if (name.ToLower() == "Маша")
+{
+    Console.WriteLine("Ура!!! Это жe Маша");
+}
+else
+{
+    Console.WriteLine("Привет");
+    Console.WriteLine(name);
+}
